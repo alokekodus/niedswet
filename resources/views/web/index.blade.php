71c7320de2a22b40de78a555385dd34c04e72c35
Alok Deka <alok.deka@ekodus.com>
2022-03-25 12:24:01 +0530

    Contact block added

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -346,17 +346,19 @@
         <h4 class="text-center fw-bold mb-5">Testimonials</h4>
         <div class="testimonial-carousel owl-carousel owl-theme">
             @for ($i = 1; $i < 5; $i++)
-            <div class="item">
-                <div class="single bg-light p-3 py-5">
-                    <div class="image">
-                        <img src="{{ asset('web_assets/images/testimonials/testi1.png') }}" alt="">
-                    </div>
-                    <div class="details">
-                        <img src="{{asset('web_assets/images/Icons/bi_quote.png')}}" alt="">
-                        <p class="text-center px-3 mb-0 text-12">{{$i}} Semper libero, porta nunc varius eros senectus. Nunc, leo facilisis dictum sed turpis vestibulum, adipiscing donec. Cursus pharetra libero, tellus egestas.</p>
+                <div class="item">
+                    <div class="single bg-light p-3 py-5">
+                        <div class="image">
+                            <img src="{{ asset('web_assets/images/testimonials/testi1.png') }}" alt="">
+                        </div>
+                        <div class="details">
+                            <img src="{{ asset('web_assets/images/Icons/bi_quote.png') }}" alt="">
+                            <p class="text-center px-3 mb-0 text-12">{{ $i }} Semper libero, porta nunc varius
+                                eros senectus. Nunc, leo facilisis dictum sed turpis vestibulum, adipiscing donec. Cursus
+                                pharetra libero, tellus egestas.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
             @endfor
         </div>
         <div class="navigation">
@@ -434,6 +436,61 @@
                                 pharetra auctor cras
                                 elementum ac.</p>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    {{-- Contact --}}
+    <section class="contact mb-5 bg-light py-5">
+        <h4 class="text-center fw-bold mb-3">Contact Us</h4>
+        <div class="container">
+            <div class="row">
+                {{-- Map --}}
+                <div class="col-sm-6 mb-3">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114612.57101497037!2d91.63284247523802!3d26.14318577239997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375a5a287f9133ff%3A0x2bbd1332436bde32!2sGuwahati%2C%20Assam!5e0!3m2!1sen!2sin!4v1648190831328!5m2!1sen!2sin"
+                        width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                    <p class="text-12">NIEdSWeT, Address Address Address Address, NIEdSWeT, Address Address Address
+                        Address, Assam, 110762</p>
+                </div>
+
+                {{-- Form --}}
+                <div class="col-sm-6">
+                    <div class="contactFormDiv">
+                        <form id="contactForm">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <input type="text" class="form-control shadow-none" id="fname" name="fname"
+                                        placeholder="First name">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control shadow-none" id="lname" name="lname"
+                                        placeholder="Last name">
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <input type="text" class="form-control shadow-none" id="phone" name="phone"
+                                    placeholder="Mobile No.">
+                            </div>
+
+                            <div class="mb-3">
+                                <input type="email" class="form-control shadow-none" id="email" name="email"
+                                    placeholder="Email">
+                            </div>
+
+                            <div class="mb-3">
+                                <textarea class="form-control shadow-none" id="message" name="message" rows="4" placeholder="Message"></textarea>
+                            </div>
+
+                            <div class="text-center">
+                                <button type="submit" class="btn submitBtn">Send</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
