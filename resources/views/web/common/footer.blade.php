86cc78a4aa0c696bb2dfdf5ace062a4c27cb8ede
Alok <alok.deka@ekodus.com>
2022-06-24 12:31:41 +0530

    Social link updated on footer

--- resources/views/web/common/footer.blade.php
+++ resources/views/web/common/footer.blade.php
@@ -10,8 +10,8 @@
                         <p>Information</p>
                         <a href="{{ route('site.about') }}">About Us</a>
                         <a href="{{ route('site.gallery.album') }}">Gallery</a>
-                        <a href="{{ route('site.privacy') }}">Privacy Policy</a>
-                        <a href="{{ route('site.terms') }}">Terms & Conditions</a>
+                        {{-- <a href="{{ route('site.privacy') }}">Privacy Policy</a> --}}
+                        {{-- <a href="{{ route('site.terms') }}">Terms & Conditions</a> --}}
                         {{-- <a href="#">Contact Us</a> --}}
                     </div>
                 </div>
@@ -40,14 +40,14 @@
                 <div class="col-sm-3 social">
                     <p>Social</p>
                     <div class="top">
-                        <img src="{{ asset('web_assets/images/Icons/facebook.png') }}" alt="Social Icons">
-                        <img src="{{ asset('web_assets/images/Icons/twitter.png') }}" alt="Social Icons">
+                        <a href="#" target="_blank"><img src="{{ asset('web_assets/images/Icons/facebook.png') }}" alt="Social Icons"></a>
+                        <a href="#" target="_blank"><img src="{{ asset('web_assets/images/Icons/youtube.png') }}" alt="Social Icons"></a>
                     </div>
 
-                    <div class="bottom">
+                    {{-- <div class="bottom">
                         <img src="{{ asset('web_assets/images/Icons/instagram.png') }}" alt="Social Icons">
                         <img src="{{ asset('web_assets/images/Icons/linkedin.png') }}" alt="Social Icons">
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
